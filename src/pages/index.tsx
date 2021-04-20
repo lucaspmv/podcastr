eb85543dedf187a468812486bd62f001206c369a
Lucas Porto <lucaspmv@hotmail.com>
2021-04-20 14:40:28 -0300

    Finalizado componentes: header e player, Configurando e consumindo API, build gerada.

---
+++ src/pages/index.tsx
@@ -0,0 +1,25 @@
+// SPA Single Page Applications
+// SSR Server Side Rendering
+// SSG Static Site Generation
+
+export default function Home(props) {
+
+  return (
+    <div>
+      <h1>Index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
+  }
+}
