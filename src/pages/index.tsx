7621b0e1367d230c48b234e871a41b3dfceca21a
Lucas Porto <lucaspmv@hotmail.com>
2021-04-23 11:55:50 -0300

    Criando contextAPI do player, implementando lógica dos butões de play e pause e acrescentando áudio no site.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,6 +11,8 @@ import ptBR from 'date-fns/locale/pt-BR';
 import { convertDurationToTimeString } from '../utils/convertDurationToTimeString';
 
 import styles from './home.module.scss';
+import { useContext } from 'react';
+import { PlayerContext } from '../contexts/PlayerContext';
 
 type Episode = {
   id: string;
@@ -29,6 +31,7 @@ type HomeProps = {
 }
 
 export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
+  const { play } = useContext(PlayerContext);
 
   return (
     <div className={styles.homePage}>
@@ -56,7 +59,7 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
                   <span>{episode.durationAsString}</span>
                 </div>
 
-                <button type="button">
+                <button type="button" onClick={() => play(episode)}>
                   <img src="/play-green.svg" alt="Tocar episódio" />
                 </button>
               </li>
